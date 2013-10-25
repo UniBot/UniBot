d5da9a55e1e5cdd7db50d434e8f0d38b4792df25
Dean Sofer <proloser@hotmail.com>
2013-10-24 21:21:01 -0700

    Initial reorganization

--- unibot.js
+++ unibot.js
@@ -4,41 +4,5 @@
  * @license MIT
  */
 
-var express  = require('express');
-var irc      = require("irc");
-var config   = require('./config');
-var commands = require("./lib/commands");
-
-var bot = new irc.Client(config.server, config.irc.userName, config.irc);
-
-commands.bot = bot;
-
-bot.addListener("registered", function(message){
-  if (config.owner && commands.bot)
-    commands.bot.say(config.owner, "Connected!");
-  commands.join();
-});
-
-bot.addListener("pm", function(from, message) {
-  var params = message.split(' ');
-  if (params.length > 1 && commands.pm[params[0]]) {
-    commands.pm[params[0]](from, params);
-  }
-});
-
-bot.addListener("message#", function(from, channel, message) {
-  var params = message.split(' ');
-  if (commands.message[params[0]]) {
-    commands.message[params[0]](from, channel, params);
-  } else if (commands.channels[channel] && commands.channels[channel].commands[params[0]]) {
-    commands.say(from, channel, params);
-  }
-});
-
-bot.addListener("error", function(error) {
-  console.log('error', error);
-  if (config.owner && commands.bot)
-    commands.bot.say(config.owner, "ERROR: "+error.args.join(' : '));
-});
-
-// app.listen(config.port);
+require('./lib/bot');
+// require('./lib/webserver');
