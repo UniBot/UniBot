1c15a537f5f3448fa04be8dbdf2d9e25a46cf1a6
Dean Sofer <proloser@hotmail.com>
2013-11-19 16:09:22 -0800

    Fixes #30
    Also refactored a little bit how plugins are setup on the webserver

--- public/js/app.js
+++ public/js/app.js
@@ -27,6 +27,10 @@ app.config(function($routeProvider){
       }
     }
   });
+}).run(function($rootScope, $http){
+  $http.get('/version').then(function(res){
+    $rootScope.version = res.data;
+  });
 });
 app.controller('ListCtrl', function($scope, channels){
   $scope.channels = channels;
@@ -38,7 +42,7 @@ app.controller('ViewCtrl', function($scope, channel){
   $scope.channel = channel;
 });
 app.controller('CommandsCtrl', function($scope, $http){
-  $http.get('/commands/'+$scope.channel._id).then(function(res){
+  $http.get('/plugins/commands/'+$scope.channel._id).then(function(res){
     $scope.commands = [];
     angular.forEach(res.data.commands, function(value, key){
       $scope.commands.push({key:key,value:value});
@@ -46,7 +50,7 @@ app.controller('CommandsCtrl', function($scope, $http){
   });
 });
 app.controller('KarmaCtrl', function($scope, $http){
-  $http.get('/karma/'+$scope.channel._id).then(function(res){
+  $http.get('/plugins/karma/'+$scope.channel._id).then(function(res){
     $scope.karmas = [];
     angular.forEach(res.data.karma, function(value, key){
       $scope.karmas.push({key:key,value:value});
@@ -54,7 +58,7 @@ app.controller('KarmaCtrl', function($scope, $http){
   });
 });
 app.controller('LogsCtrl', function($scope, $http){
-  $http.get('/logs/'+$scope.channel._id).then(function(res){
+  $http.get('/plugins/logs/'+$scope.channel._id).then(function(res){
     $scope.logs = res.data && res.data.logs;
   });
   $scope.currentPage = 1;
--- unibot.js
+++ unibot.js
@@ -9,10 +9,5 @@ config = require('./config');
 
 require('./lib/bot');
 
-// @TODO Refactor approach of adding plugin routes to webserver
-require('./lib/plugins/commands');
-require('./lib/plugins/karma');
-require('./lib/plugins/logs');
-
 webserver = require('./lib/webserver');
 webserver.listen(config.port);
